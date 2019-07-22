7e2770b27bc07d007219e2b9ded2bf473097c58f
williamorim <thewilliam89@gmail.com>
2019-07-22 17:13:28 -0300

    nova aula de importacao

---
+++ scripts/01-importacao.R
@@ -0,0 +1,190 @@
+
+
+# Funções -----------------------------------------------------------------
+
+#Sintaxe
+nome_da_funcao(arg1, arg2)
+
+# Objetos -----------------------------------------------------------------
+
+# Os nomes devem começar com uma letra. Podem conter letras, números, _ e .
+
+eu_uso_snake_case
+outrasPessoasUsamCamelCase
+algumas.pessoas.usam.pontos
+E_algumasPoucas.Pessoas_RENUNCIAMconvenções
+
+obj <- 1
+obj
+
+# ATALHO para rodar o código: CTRL + ENTER
+# ATALHO para a <- : ALT - (alt menos)
+
+# O R difencia minúscula de maiúscula!
+
+a <- 5
+A <- 42
+
+
+
+# Caminhos até o arquivo --------------------------------------------------
+
+# 1. Podem ser absolutos
+"/home/william/Documents/Curso-R/intro-programacao-em-r-mestre/dados/imdb.csv"
+
+# 2. Podem ser relativos ao diretório de trabalho
+getwd()
+
+
+
+# Importando de texto e excel ---------------------------------------------
+
+# Arquivos de texto
+imdb <- read_csv(file = "dados/imdb.csv")
+imdb2 <- read_delim("dados/imdb2.csv", delim = ";")
+
+# Excel
+library(readxl)
+imdb_excel <- read_excel("dados/imdb.xlsx")
+
+
+
+
+# Classes -----------------------------------------------------------------
+
+# Numéricos (numeric)
+
+obj <- 10
+class(obj)
+
+# Caracteres (character, strings)
+
+obj <- "s"
+obj2 <- "sim"
+obj3 <- "a resposta é sim"
+
+class(obj)
+class(obj2)
+class(obj3)
+
+# Lógicos
+
+obj <- TRUE
+
+# Bases (data.frame)
+
+class(imdb)
+
+
+
+# Lendo vários arquivos ---------------------------------------------------
+
+imdb_2013 <- read_csv("dados/por-ano/imdb-2013.csv")
+imdb_2014 <- read_csv("dados/por-ano/imdb-2014.csv")
+imdb_2015 <- read_csv("dados/por-ano/imdb-2015.csv")
+
+imdb <- rbind(imdb_2013, imdb_2014, imdb_2015)
+
+
+
+
+# Vetores -----------------------------------------------------------------
+
+c(1, 4, 3, 10)
+
+1:10
+
+# Subsetting
+
+vetor <- c(4, 8, 15, 16, 23, 42)
+
+vetor[1]
+vetor[c(1, 3)]
+vetor[-5]
+vetor[-c(1, 3)]
+
+
+# Coerção
+
+class(c(1, 2, 3))
+
+c(1, 2, 3, "a")
+c(TRUE, FALSE, "a")
+c(1L, 2L, "a")
+c(TRUE, FALSE, 1)
+
+
+
+# Controle de fluxo -------------------------------------------------------
+
+# if/else
+x <- 0
+
+if(x < 0) {
+  "negativo"
+} else if(x == 0) {
+  "neutro"
+} else {
+  "positivo"
+}
+
+# for
+for(i in 1:10) {
+  texto <- paste("Essa é a repetição", i)
+  print(texto)
+}
+
+# Lendo vários arquivos 2 -------------------------------------------------
+
+arquivos <- list.files("dados/por-ano/", full.names = TRUE)
+  
+for(caminho in arquivos) {
+  
+  base <- read_csv(caminho)
+  
+  if(caminho == arquivos[1]) {
+    base_final <- base
+  } else {
+    base_final <- rbind(base_final, base)
+  }
+  
+}
+  
+
+# SQL ---------------------------------------------------------------------
+
+library(dplyr)
+
+conexao <- src_sqlite("dados/imdb.sqlite", create = TRUE)
+
+imdb_sqlite <- tbl(conexao, "imdb")
+imdb_select <- tbl(conexao, sql("SELECT titulo, ano, diretor FROM imdb"))
+
+# trazer para a memória
+collect(imdb_sqlite)
+collect(imdb_select)
+
+# db.rstudio.com
+
+# Outros formatos ---------------------------------------------------------
+
+library(jsonlite)
+imdb_json <- read_json("dados/imdb.json")
+
+library(haven)
+
+imdb_sas <- read_sas("dados/imdb.sas7bdat")
+imdb_spss <- read_spss("dados/imdb.sav")
+
+# pacote rio
+library(rio)
+imdb_rio <- rio::import("dados/imdb.xlsx")
+
+# data.table
+library(data.table)
+imdb_dt <- fread("dados/imdb.csv")
+
+
+
+
+  
